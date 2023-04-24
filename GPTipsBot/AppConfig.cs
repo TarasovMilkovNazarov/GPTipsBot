8e22eabc7482b5e059c41d52e7213f635c492d8a
Aleksandr Tarasov <alan.trsv@gmail.com>
2023-04-24 17:46:15 +0500

    Reverted environment variables

--- GPTipsBot/AppConfig.cs
+++ GPTipsBot/AppConfig.cs
@@ -9,24 +9,30 @@ namespace GPTipsBot
     public static class AppConfig
     {
         public static string Env { get; private set; }
-        public static string TelegramToken { get; }
-        public static string OpenAiToken { get; }
+        public static string TelegramToken { get; private set; }
+        public static string OpenAiToken { get; private set; }
+        public static string PG_CONNECTION_STRING { get; private set; }
+
         public const long СhatId = 486363646;
 
         static AppConfig()
         {
-            Env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "dev";
-            TelegramToken = Environment.GetEnvironmentVariable("TELEGRAM_TOKEN") ?? "";
-            OpenAiToken =  Environment.GetEnvironmentVariable("OPENAI_TOKEN") ?? "";
-            
-            if (TelegramToken == "")
-            {
-                throw new ArgumentNullException("Telegram is not configured");
-            }
-            if (OpenAiToken == "")
-            {
-                throw new ArgumentNullException("Chat GPT is not configured");
-            }
+            TelegramToken = "6272630353:AAG6zDC3BTBQ0dt09nHE6_mN4RpDRUEjPDM";
+            OpenAiToken = "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";
+            PG_CONNECTION_STRING="Host=localhost;Port=5432;Database=gptips;Username=postgres;Password=postgres";
+
+            Env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+            //TelegramToken = Environment.GetEnvironmentVariable("TELEGRAM_TOKEN") ?? "";
+            //OpenAiToken =  Environment.GetEnvironmentVariable("OPENAI_TOKEN") ?? "";
+
+            //if (TelegramToken == "")
+            //{
+            //    throw new ArgumentNullException("Telegram is not configured");
+            //}
+            //if (OpenAiToken == "")
+            //{
+            //    throw new ArgumentNullException("Chat GPT is not configured");
+            //}
         }
     }
 }
