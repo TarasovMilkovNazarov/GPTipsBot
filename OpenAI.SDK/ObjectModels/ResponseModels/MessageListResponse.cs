12939bdd18e3ca41c0b3a2aeca7c126061fc4a39
Aleksandr Tarasov <alan.trsv@gmail.com>
2024-05-28 18:52:28 +0500

    added open ai library directly(fixed error with CreatedAt)

---
+++ OpenAI.SDK/ObjectModels/ResponseModels/MessageListResponse.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+using OpenAI.ObjectModels.SharedModels;
+
+namespace OpenAI.ObjectModels.ResponseModels;
+
+public record MessageListResponse : DataWithPagingBaseResponse<List<MessageResponse>>
+{
+}
