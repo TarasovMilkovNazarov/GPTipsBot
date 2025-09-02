a4e2f9c9d2805da869acbb2ba8681e75b25f824f
Aleksandr Tarasov <tarasov.aa@lime-shop.com>
2025-09-02 04:23:42 +0000

    added free requests due to low level of usage

--- GPTipsBot/Config/PaymentConfig.cs
+++ GPTipsBot/Config/PaymentConfig.cs
@@ -10,14 +10,14 @@ public static class PaymentConfig
     /// <summary>
     /// Число бесплатных запросов на генерацию изображений, которое добавляется джобой обновления лимитов
     /// </summary>
-    public const int FreeImageGenerations = 5;
-    public const int FreeTextRecognitions = 5;
+    public const int FreeImageGenerations = 10;
+    public const int FreeTextRecognitions = 10;
     public const int FreeChatGptRequests = 10;
     /// <summary>
     /// Число бесплатных запросов для новых пользователей
     /// </summary>
-    public const int NewbieFreeImageGenerations = 3;
-    public const int NewbieFreeTextRecognitions = 3;
+    public const int NewbieFreeImageGenerations = 10;
+    public const int NewbieFreeTextRecognitions = 10;
     public const int NewbieFreeChatGptRequests = 10;
     public const int MinRechargeAmount = 10;
     public const int Cartoonify = 8;
