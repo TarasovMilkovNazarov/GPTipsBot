d8d34c458f62b7a776d789f90a19bad1ff6a3e58
Aleksandr Tarasov <tarasov.aa@lime-shop.com>
2026-07-07 04:24:30 +0000

    got rid of unnecessary env variables

--- GPTipsBot/Config/AppConfig.cs
+++ GPTipsBot/Config/AppConfig.cs
@@ -10,10 +10,6 @@ namespace GPTipsBot.Config
         public static readonly bool IsDevelopment = Env != "Production";
         public static readonly bool IsProduction = Env == "Production";
         public static string TelegramToken => GetEnvStrict("TELEGRAM_TOKEN");
-        public static string ProxyLogin => GetEnvStrict("PROXY_LOGIN");
-        public static string ProxyPwd => GetEnvStrict("PROXY_PWD");
-        public static string ProxyIp => GetEnvStrict("PROXY_IP");
-        public static string ProxyPort => GetEnvStrict("PROXY_PORT");
         public static string ConnectionString => GetEnvStrict("PG_CONNECTION_STRING");
         public static string Env => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
         public static string? Version => Environment.GetEnvironmentVariable("GPTIPSBOT_VERSION");
@@ -27,8 +23,6 @@ namespace GPTipsBot.Config
         public static string YandexIamToken => Environment.GetEnvironmentVariable("YC_IAM_TOKEN");
         public static string YandexCloudFolderId => GetEnvStrict("YC_FOLDER_ID");
         public static string? TelejetApiKey => Environment.GetEnvironmentVariable("TELEJET_API_KEY");
-        public static string ProxyApiApiKey => Environment.GetEnvironmentVariable("WWW_PROXY_API_API_KEY");
-        public static string PawanOsmanApiKey => Environment.GetEnvironmentVariable("PAWAN_OSMAN_API_KEY");
         public static string? GramadsBearerToken => Environment.GetEnvironmentVariable("GRAMADS_BEARER");
         public static string? HappSubscriptionUrl => Environment.GetEnvironmentVariable("HAPP_SUBSCRIPTION_URL");
         public static string HappProxyIp => Environment.GetEnvironmentVariable("HAPP_PROXY_IP") ?? "127.0.0.1";
